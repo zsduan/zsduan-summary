a4558ac55ad6e2ff0543f62a7dc7b4152ddea03b
zs.duan <zs.duan@qq.com>
2023-04-23 17:03:49 +0800

    新增 表格可以选择可以展示那些按钮了

---
+++ src/components/dzs-table/sider/dzs-tabel-sider.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="tabel-sider-box">
+        <el-popover :offset="60" placement="left-start" width="80%" trigger="click">
+            <div :style="{...titleStyle}">列设置</div>
+            <el-checkbox-group v-model="goupList">
+                <el-checkbox v-for="(item,index) in columnList" :checked="item.isShow" :key="index" :label="item" @change="changeCheckbox(index)">{{item.lable}}</el-checkbox>
+            </el-checkbox-group>
+            <div slot="reference" class="el-icon">
+                <i class="el-icon-setting"></i>
+            </div>
+        </el-popover>
+        <el-popover :offset="30" placement="left-start" width="80%" trigger="click">
+            <div class="title" :style="{...titleStyle}">表格栏高度设置</div>
+            <el-radio-group v-model="radio" @change="changeHeight">
+                <el-radio-button v-for="(item,index) in widthList" :key="index" :label="item.value">{{item.label}}</el-radio-button>
+            </el-radio-group>
+            <div slot="reference" class="el-icon">
+                <i class="el-icon-s-operation"></i>
+            </div>
+        </el-popover>
+    </div>
+</template>
+<script>
+export default {
+    name: "dzs-tabel-sider",
+    props: {
+        list: {
+            type: Array,
+            default: () => {
+                return [];
+            },
+        },
+    },
+    computed: {
+        returnList() {
+            return this.columnList.filter((item) => {
+                return item.isShow;
+            });
+        },
+    },
+    data() {
+        return {
+            columnList: [],
+            goupList: [],
+            widthList : [
+                {
+                    label : '自动',
+                    value : '',
+                },
+                {
+                    label : '小',
+                    value : 'mini',
+                },
+                {
+                    label : '中',
+                    value : 'middle',
+                },
+                {
+                    label : '大',
+                    value : 'max',
+                },
+            ],
+            radio : '',
+            titleStyle : {
+                padding: '5px 0',
+                'border-bottom': '1px solid #ebeef5',
+                'font-size': '14px',
+                color: '#606266',
+                'margin-bottom': '10px',
+            },
+        };
+    },
+    watch: {
+        list: {
+            handler(val) {
+                if (!val.length) return;
+                this.columnList = JSON.parse(JSON.stringify(val));
+                this.columnList.forEach((item) => {
+                    item.isShow = true;
+                });
+            },
+            deep: true,
+            immediate: true,
+        },
+    },
+    methods : {
+        changeCheckbox(index) {
+            this.columnList[index].isShow = !this.columnList[index].isShow;
+            this.$set(this.columnList, index, this.columnList[index]);
+            this.$emit("change", this.returnList);
+        },
+        /**改变高度*/
+        changeHeight() {
+            let index = this.widthList.findIndex((item) => {
+                return item.value === this.radio;
+            });
+            console.log("index ==>" ,index);
+            window.localStorage.setItem("tableHeight", this.widthList[index].value);
+            this.$emit("changeHeight", this.widthList[index].value);
+        }, 
+    }
+};
+</script>
+<style lang="less" scoped>
+.tabel-sider-box {
+    width: 30px;
+    height: 100%;
+    background-color: var(--ThemeColor01);
+    .el-icon {
+        padding: 10px 0;
+        text-align: center;
+    }
+    .el-icon-s-operation,.el-icon-setting {
+        color: rgb(124, 124, 124);
+        font-size: 20px;
+        cursor: pointer;
+    }
+}
+</style>
--- src/views/table/config.js
+++ src/views/table/config.js
@@ -14,6 +14,9 @@ export const TABALE_HEADER = [
     {
         lable: "排序",
         key: "sorts",
+        props : {
+            sortable : true
+        }
     },
     {
         lable: "是否显示",
@@ -30,6 +33,31 @@ export const operation = ["edit" , 'detail' , 'detele'];
 
 export const code1 = 
 `
+/**
+ * @name 表格组件
+ * @props tableList 列表数据 isSlot:自定义slot
+ * @props operation 操作数据 add 新增 edit 编辑 details 详情 del 删除
+ * @props tableHeader 表头数据及表单key  lable 表头名称  key 显示关键字 width 宽度 isSlot:自定义slot props:el-table-column的配置项
+ * @props showCheckbox 是否显示多选 默认 false 不显示  true 显示
+ * @props maxSize 每页显示条数 默认10
+ * @props showPagination 是否显示分页 默认true 显示
+ * @props operationOption 操作列配置项
+ * @props tableData 表格数据
+ * @props total 总条数
+ * @props tableOptions 饿了吗ui的配置
+ * @props loading 是否开启加载中 可以在 tableOptions配置自己想要的加载 element-loading-text="拼命加载中" element-loading-spinner="el-icon-loading"
+ *
+ * @methods SelectionChange 选中数据改变 返回当前选中的数据
+ * @methods change 页码/条数改变 返回当前页码/条数  status:page 改变页码  status:size 改变条数
+ * @methods change 列表数据改变 返回当前改变的数据  status:update 更新数据  status:delete 删除数据  status:edit 编辑数据 status:details 详情数据
+ * @methods onEdit 编辑
+ * @methods onDetails 详情
+ * @methods onDel 删除
+ * @methods setSelection 设置选中的数据
+ *
+ */
+
+
 html
 <dzs-table :tableHeader="TABALE_HEADER" :tableData="tableData" :operation="operation">
     <template #is_show="rowData">
